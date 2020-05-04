8f2ca804b51294a9d428b4c00dcd590881598a4c
Unknown <unknown@example.com>
2020-05-04 22:01:53 +0200

    Initialize test variables

--- api_app/tests.py
+++ api_app/tests.py
@@ -8,3 +8,19 @@ from api_app.models import Co2
 
 class Co2Api(APITestCase):
 
+    def setUp(self):
+
+        self.valid_co2 = {
+            'datetime': '2017-01-01T01:30:00',
+            'co2_rate': 33
+        }   
+        
+        self.invalid_co2 = {
+            'datetime': '2017-01-01T01:30:00',
+            'co2_rate': 33.74
+        }
+
+
+        self.detail_url = reverse('co2Detail', kwargs={'pk': 15}) 
+        self.list_url = reverse('co2List')
+
