eeffe446713cd445e31dddc5dd5f17b7666baf70
Unknown <unknown@example.com>
2020-05-04 21:56:35 +0200

    Define django admin page for co2 model

---
+++ api_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Co2
+
+class Co2Admin(admin.ModelAdmin):
+    list_display = ('datetime', 'co2_rate')
+   
+
+admin.site.register(Co2, Co2Admin)
