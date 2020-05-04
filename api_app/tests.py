5fb5c021fc248e04af9ef5b5719f35430c64e83a
Unknown <unknown@example.com>
2020-05-04 22:00:02 +0200

    import useful package in test file

---
+++ api_app/tests.py
@@ -0,0 +1,8 @@
+from rest_framework import status
+from django.urls import reverse  
+from rest_framework.test import APITestCase
+from .views import Co2Serializer
+import json
+from api_app.models import Co2
+
+
