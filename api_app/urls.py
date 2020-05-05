fc6a496dbffa42e53ede26a3fbeab436f42863a2
Unknown <unknown@example.com>
2020-05-05 13:29:44 +0200

    Uncomment load data url

--- api_app/urls.py
+++ api_app/urls.py
@@ -11,6 +11,6 @@ from django.urls import path
 urlpatterns = [
     path('co2List/', co2_list, name='co2List'),  # path to get all co2 datas (/api_app/co2List)
     path('co2Detail/<int:pk>/', co2_detail, name='co2Detail'),  # path to get a single data object (/api_app/co2detail)
-    # path('datas/', get_data, name='get_data'),  # path to load api data in database; when running the project for the first time, uncomment this line
+    path('datas/', get_data, name='get_data'),  # path to load api data in database; 
     path('panda/', pandaPart, name='panda'),  # path to get results for pandaPart view function (/api_app/panda)
 ]
