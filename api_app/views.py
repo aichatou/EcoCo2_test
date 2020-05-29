7c89c45ce6500aff7a5fb34684b6d7b17b069b6e
Unknown <unknown@example.com>
2020-05-29 10:01:13 +0200

    import render for template creation

--- api_app/views.py
+++ api_app/views.py
@@ -6,6 +6,7 @@ import requests
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import HttpResponse
+from django.shortcuts import render
 from rest_framework.decorators import api_view
 import matplotlib.pyplot as plt
 
