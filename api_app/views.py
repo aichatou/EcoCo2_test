c3122da1dee9640055ae474acce316556c920aac
Unknown <unknown@example.com>
2020-05-29 10:05:26 +0200

    Define template data

--- api_app/views.py
+++ api_app/views.py
@@ -131,17 +131,16 @@ def pandaPart(request):
     csv_data_interpol = csv_data_df2.resample('15T').mean().interpolate('linear')
     csv_data_interpol.to_csv('csv_data_interpol.csv', sep=';')
     # calculate co2 production
-    co2Prod = data_interpol.sum()
+    co2Prod = csv_data_interpol.sum()
 
-    text = "<h1> Successful operations<br> Results files (co2data.png, csv_data_interpol.csv, data_interpol.csv )can be shown in api_project folder<br> </h1>"
-    text += "<h1> Consommation Co2: " + str(list(co2Prod)[0]) + "</h1>"
-    text += "<h1> Moyenne weekday: " + \
-        str(list(Weekday_data)[0]) + "</h1>" + "<h1> Moyenne weekend: " + str(list(Weekend_data)[0]) + "</h1>"
-    text += "<h1> Moyenne weekday (interpolated data): " + str(list(Weekday_interpol)
-                                                               [0]) + "</h1>" + "<h1> Moyenne weekend (interpolated data): " + str(list(Weekend_interpol)[0]) + "</h1>"
-    text += "<h1> Mediane season: Winter, Spring, Summer, Fall:  " + \
-        str(list(Winter)[0]) + ", " + str(list(Spring)[0]) + ", " + \
-        str(list(Summer)[0]) + ", " + str(list(Fall)[0]) + "</h1>"
-    text += "<h1> Mediane season (interpolated data): Winter, Spring, Summer, Fall: " + str(list(Winter_interpol)[0]) + ", " + str(
-        list(Spring_interpol)[0]) + ", " + str(list(Summer_interpol)[0]) + ", " + str(list(Fall_interpol)[0]) + "</h1>"
-    return HttpResponse(text)
+    co2Produc = list(co2Prod)[0]
+    mean_week = [list(Weekday_data)[0], list(Weekend_data)[0]]
+    interpolated_week = [list(Weekday_interpol)[0], list(Weekend_interpol)[0]]
+    median_season = [list(Winter)[0], list(Spring)[0], list(Summer)[0], list(Fall)[0]]
+    interpolated_median_season = [list(Winter_interpol)[0], list(Spring_interpol)[0],
+                                  list(Summer_interpol)[0], list(Fall_interpol)[0]]
+
+    resulting_data = {'co2Produc': co2Produc, 'mean_week': mean_week, 'interpolated_week': interpolated_week, 'median_season': median_season,
+                      'interpolated_median_season': interpolated_median_season}
+
+    return render(request, 'panda_result.html', resulting_data)
